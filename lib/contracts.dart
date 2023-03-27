b641f2e834dc9091b9502c411bbbbcbc6f52a3ec
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-03-27 07:26:53 +0200

    feat: Create library entrypoint

---
+++ lib/contracts.dart
@@ -0,0 +1,8 @@
+/// Global Mineral's core contracts
+library contracts;
+
+export 'src/contracts/mineral_package_contract.dart';
+export 'src/contracts/event_service_contract.dart';
+export 'src/contracts/command_service_contract.dart';
+export 'src/contracts/shared_state_service_contract.dart';
+export 'src/contracts/context_menu_service_contract.dart';
